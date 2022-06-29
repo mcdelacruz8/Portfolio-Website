3aa2303815fed107bb2bf9d86efbbec7b346bc6d
mcdelacruz8 <88753390+mcdelacruz8@users.noreply.github.com>
2022-06-29 14:28:46 -0700

    Finished Style Selector
    1. Light/Dark Mode feature
    2. Text Animation feature

---
+++ portfolio_site/js/script.js
@@ -0,0 +1,7 @@
+/* ============================= Typing Animation ============================= */
+var typed = new Typed(".typing", {
+  strings: ["", "Web Developer", "Basketball Fan", "Anime Enthusiast", "Cool Guy"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true
+})
